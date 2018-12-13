cdab3b55b5f26331d0367ae9f3f3ab2968f0ea42
tiendat <tiendatdeveloper1997@gmail.com>
2018-12-13 10:17:08 +0700

    Add 2 store procedure to project DB

---
+++ StyleViet.Database/dbo/Stored Procedures/RegisterMemberAccount.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[RegisterMemberAccount]
+(
+	@username NVARCHAR(MAX),  
+    @password NVARCHAR(MAX),  
+    @email NVARCHAR(MAX),  	
+    @firstname NVARCHAR(MAX),  
+	@lastname NVARCHAR(MAX),
+	@phone NVARCHAR(MAX)
+)
+AS
+BEGIN
+	DECLARE @result VARCHAR(10) = 'Failed'
+	DECLARE @newAccId INT 
+	DECLARE @roleMember INT = 3	
+
+	IF NOT EXISTS(SELECT 1 FROM Account a where a.Username = @username or UPPER(a.Email) = UPPER(@email))
+	BEGIN
+		INSERT INTO Account(Username, Password, Email, Expired)  
+		VALUES(@username,@password,@email,0)
+
+		SET @newAccId = (SELECT a.Id from Account a where a.Username = @username and a.Password = @password)
+		IF  (@newAccId IS NOT NULL)
+		BEGIN
+			INSERT INTO AccountRole(AccountId,RoleId)
+			VALUES(@newAccId, @roleMember)
+
+			INSERT INTO [dbo].[User](AccountId,Email,Phone,FirstName,LastName)
+			VALUES(@newAccId,@email,@phone,@firstname,@lastname)
+			SET @result='Success'
+		END 		
+	END
+	SELECT @result AS Result  
+END
+GO
+
+
---
+++ StyleViet.Database/dbo/Stored Procedures/RegisterSalonAccount.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[RegisterSalonAccount]
+(
+	@username NVARCHAR(MAX),  
+    @password NVARCHAR(MAX),  
+    @email NVARCHAR(MAX),  	
+    @salonname NVARCHAR(MAX),  
+	@phone NVARCHAR(MAX),
+	@address NVARCHAR(MAX)
+)
+AS
+BEGIN
+	DECLARE @result VARCHAR(10) = 'Failed'
+	DECLARE @newAccId INT 
+	DECLARE @roleSalon INT = 2	
+
+	IF NOT EXISTS(SELECT 1 FROM Account a where a.Username = @username or UPPER(a.Email) = UPPER(@email))
+	BEGIN
+		INSERT INTO Account(Username, Password, Email, Expired)  
+		VALUES(@username,@password,@email,0)
+
+		SET @newAccId = (SELECT a.Id from Account a where a.Username = @username and a.Password = @password)
+		IF  (@newAccId IS NOT NULL)
+		BEGIN
+			INSERT INTO AccountRole(AccountId,RoleId)
+			VALUES(@newAccId, @roleSalon)
+
+			INSERT INTO [dbo].[Salon](AccountId,Name,Phone,Address,Email)
+			VALUES(@newAccId,@salonname,@phone,@address,@email)
+			SET @result='Success'
+		END 		
+	END
+	SELECT @result AS Result  
+END
+GO
+
+
